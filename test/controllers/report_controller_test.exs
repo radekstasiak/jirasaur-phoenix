408e30415658b93825fa94ca7941a5e125fdb5d3
Radek <radek.stasiak@gmail.com>
2017-04-13 23:06:29 +0200

    Add tests for slack authentication
    - test if request authenticated with correct token
    - test if request unauthorized when token is incorrect

--- test/controllers/report_controller_test.exs
+++ test/controllers/report_controller_test.exs
@@ -1,4 +1,15 @@
 defmodule Jirasaur.ReportControllerTest do
   use Jirasaur.ConnCase
 
+  test "request authenticated with correct SLACK TOKEN", %{conn: conn} do
+  	System.put_env("SLACK_TOKEN", "aaa")
+    conn = post conn, api_v1_report_path(conn, :process_request), token: "aaa"
+    assert conn.status == 200
+  end
+
+    test "request unauthorized when incorrect SLACK TOKEN", %{conn: conn} do
+  	System.put_env("SLACK_TOKEN", "aaa")
+    conn = post conn, api_v1_report_path(conn, :process_request), token: "bbb"
+    assert conn.status == 401
+  end
 end
