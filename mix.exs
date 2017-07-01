82cd97bc2172fb0d68d37c6a9fba251d381a23e7
Radek <radek.stasiak@gmail.com>
2017-07-01 22:03:56 +0100

    Update coveralls settings to fix travis issue

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Jirasaur.Mixfile do
      aliases: aliases(),
      deps: deps(),
      test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]]
+     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test, "coveralls.json": :test]]
   end
 
   # Configuration for the OTP application.
